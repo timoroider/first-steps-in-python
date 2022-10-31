ceea64ef9be000ebbed73107cd20fde70e56a6a6
Michael Wettstein <chischte@gmx.ch>
2022-10-31 10:58:14 +0100

    Add assignment weather report

---
+++ src/weather-report.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# EXERCISE WEATHER REPORT
+# ------------------------------------------------------------------------------
+# • Write code that creates a sentence containing the information of the days
+#   weather and peak temperature.
+# • The weather information will be provided from the weather station as a
+#   variable of type string such as "sunny" or "clouded" and a temperature
+#   value of type int.
+# • The complete sentence should be printed to the console.
+# ------------------------------------------------------------------------------
