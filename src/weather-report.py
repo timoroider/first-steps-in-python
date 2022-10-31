430b8aa7c267995bbb912054a567d2f01a2430db
Timo Roider <timo.roider@gmail.com>
2022-10-31 17:47:13 +0100

    Complete assingment weather repot

--- src/weather-report.py
+++ src/weather-report.py
@@ -8,3 +8,20 @@
 #   value of type int.
 # • The complete sentence should be printed to the console.
 # ------------------------------------------------------------------------------
+
+
+# Define variables from weather station
+
+weather_today = "cloudy"
+temperature_today = 20
+
+
+# Create sentence
+
+# sentence = "Today it's sunny and the temperature is 33°C"
+empty_sentence = "Today it's %s and the temperature is %d °C"
+weather_sentence = empty_sentence % (weather_today, temperature_today)
+
+# Create output
+
+print(weather_sentence)
