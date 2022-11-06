207063ea731ba2a9d739436c6dac40d688b57285
timoroider <116829780+timoroider@users.noreply.github.com>
2022-11-06 11:58:37 +0100

    Learn some math

---
+++ src/math.py
@@ -0,0 +1,46 @@
+# ------------------------------------------------------------------------------
+# EXCERCISES FROM https://www.youtube.com/watch?v=T6OLDHAWjjA
+# ------------------------------------------------------------------------------
+
+name = "Timo"
+
+age = 13
+
+actual_age = 13.7
+
+# print(name)
+# print(age)
+
+
+# print(type(name))
+# print(type(age))
+# print(type(actual_age))
+
+
+# Calculations
+
+print(5 + 7)
+print(5 - 7)
+print(5 / 7)
+print(5 * 7)
+print(5 ** 7)
+
+print(5 ** 7 + 6 / 9 * 6)
+
+math = 5 ** 7 + 6 / 9 * 6
+
+print(math)
+
+result = age + actual_age + math
+
+print(result)
+
+# OPERATION   OPERATOR
+# add ------> +
+# sub ------> -
+# mult -----> *
+# div ------> /
+# exp ------> **
+
+
+
--- src/robot_barista.py
+++ src/robot_barista.py
@@ -3,26 +3,39 @@
 # ------------------------------------------------------------------------------
 
 # Welcome to the coffee shop
-print("Hello, welcome to NetworkChuckCoffe!!!!!!!!!!!!!!!!!!!!!")
+print("Hello, welcome to NetworkChuckCoffe!")
 
 # Get name and greet
 name = input("What is your name? \n")
 
 print("Hello " + name + " , thank you so much for coming in today!")
 
+# Price
+price_food = 50
+price_beverages = 4
+
+
 # Order Food
-print("""Food: Pizza 
+print("""Food:
+Pizza 
 Salad
 Burger""")
 
 food = input("What do you want to eat? \n")
 
 # Order beverage
-print("""beverages:\n Coffe
+print("""beverages:
+Coffe
 Cola
 Sirup""")
 
 drink = input("What do you want to drink? \n")
 
+count_beverage = int(input("How many " + drink + "s would you like to order?"))
+
 # Summarize order
-print("Ok " + name + " your " + food + " and your " + drink + " will be ready in a few minutes")
+print("Ok " + name + " your " + food + " and your " + str(count_beverage) + " " + drink + "s will be ready in a few minutes")
+
+price = count_beverage * price_beverages + price_food
+
+print("That would be $" + str(price) + " please")
