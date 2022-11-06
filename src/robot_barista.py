4c50c0b4eba8a725dba5c8aa6007c7d681d974b3
timoroider <116829780+timoroider@users.noreply.github.com>
2022-11-06 14:40:24 +0100

    Add if statement

--- src/robot_barista.py
+++ src/robot_barista.py
@@ -8,34 +8,38 @@ print("Hello, welcome to NetworkChuckCoffe!")
 # Get name and greet
 name = input("What is your name? \n")
 
-print("Hello " + name + " , thank you so much for coming in today!")
 
-# Price
-price_food = 50
-price_beverages = 4
+# Lock out Ben
+if name == "Ben":
+    print("You're not welcome here evil Ben!! Get out!!")
+else:
+    print("Hello " + name + " , thank you so much for coming in today!")
 
+    # Price
+    price_food = 50
+    price_beverages = 4
 
-# Order Food
-print("""Food:
-Pizza 
-Salad
-Burger""")
+    # Order Food
+    print("""Food:
+    Pizza 
+    Salad
+    Burger""")
 
-food = input("What do you want to eat? \n")
+    food = input("What do you want to eat? \n")
 
-# Order beverage
-print("""beverages:
-Coffe
-Cola
-Sirup""")
+    # Order beverage
+    print("""beverages:
+    Coffe
+    Cola
+    Sirup""")
 
-drink = input("What do you want to drink? \n")
+    drink = input("What do you want to drink? \n")
 
-count_beverage = int(input("How many " + drink + "s would you like to order?"))
+    count_beverage = int(input("How many " + drink + "s would you like to order?"))
 
-# Summarize order
-print("Ok " + name + " your " + food + " and your " + str(count_beverage) + " " + drink + "s will be ready in a few minutes")
+    # Summarize order
+    print("Ok " + name + " your " + food + " and your " + str(count_beverage) + " " + drink + "s will be ready in a few minutes")
 
-price = count_beverage * price_beverages + price_food
+    price = count_beverage * price_beverages + price_food
 
-print("That would be $" + str(price) + " please")
+    print("That would be $" + str(price) + " please")
